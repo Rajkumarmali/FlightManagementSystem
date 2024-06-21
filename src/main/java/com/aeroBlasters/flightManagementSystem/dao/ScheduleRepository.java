579899c4ecd2ec32679f1b35aaa15f3b4805edbb
Vijay Yadav <120326151+ajayvijay9929@users.noreply.github.com>
2024-06-21 23:47:33 +0530

    Delete src directory

--- src/main/java/com/aeroBlasters/flightManagementSystem/dao/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aeroBlasters.flightManagementSystem.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aeroBlasters.flightManagementSystem.bean.Schedule;
-public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
-
-}
